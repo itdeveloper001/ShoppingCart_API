15bfcd673f8c64f7866d527601e943801c0daa1d
itdeveloper <aqibaptechsfc105@gmail.com>
2017-09-10 00:59:44 -0700

    Added shoppnig cart api with template

---
+++ ShoppingCart_API/Controllers/ShoppingCartController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ShoppingCart_API.Models;
+
+namespace ShoppingCart_API.Controllers
+{
+    public class ShoppingCartController : ApiController
+    {
+
+        [Route("api/GetProducts")]
+        [HttpGet]
+        public List<Product> GetProducts()
+        {
+            ShoppingCartEntities1 db = new ShoppingCartEntities1();
+            
+            
+            db.Configuration.ProxyCreationEnabled = false;
+
+            return db.Products.ToList();
+        }
+        
+    }
+}
